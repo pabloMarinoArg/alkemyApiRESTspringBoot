2a2b6b7a323f2253634eb4433fb3d53f711e4401
pablomarinoarg <pablojesusmarino@gmail.com>
2021-08-08 01:09:34 -0300

    agrego Okta Sec

--- src/main/java/com/javaspringboot/api/alkemyjavaspringboot/api/auth/SpringSecurityConfig.java
+++ src/main/java/com/javaspringboot/api/alkemyjavaspringboot/api/auth/SpringSecurityConfig.java
@@ -4,27 +4,48 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.okta.spring.boot.oauth.Okta;
+
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
-	
-	@Autowired
-	private UserDetailsService usuarioService;
-	
-	@Bean
-	public BCryptPasswordEncoder passwordEnconder() {
-		return new BCryptPasswordEncoder();
-	}
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	@Override
-	@Autowired
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(this.usuarioService).passwordEncoder(passwordEnconder());
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		//protect endpoint
+		http.authorizeRequests()
+			.antMatchers("/genero/**","/characters/**","/movies/**","/user/**")
+			.authenticated()
+			.and()
+			.oauth2ResourceServer()
+			.jwt();
+		
+		
+		
+		//add CORS filter
+		http.cors();
+		
+		// forces a non-empty response body for 401's 
+		Okta.configureResourceServer401ResponseBody(http);
+		
 	}
 	
+
 	
 	
 
